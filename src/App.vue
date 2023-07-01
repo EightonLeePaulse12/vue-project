07c87639990e6e971020ff8437dacfec88e4fce0
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-07-01 12:54:33 +0200

    NAVBAR AND FOOTER COMPLETE, WILL EDIT LATER

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Navbar />
+  <router-view />
+  <Footer/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  *,
+  *::before,
+  *::after{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  html,
+  body{
+    overflow-x:hidden;
+  }
+
+  body{
+    text-align:center;
+    font-family: 'Poppins', sans-serif;
+  }
 </style>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 createApp(App).use(store).use(router).mount('#app')
