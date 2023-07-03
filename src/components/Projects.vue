eaaafd7157ec657ba133790cb24bf220373374c8
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-07-03 14:49:18 +0200

    PROJECTS DONE, WILL STYLE LATER

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -26,11 +26,16 @@
                                 <img id="picture" :src="item.image" alt="Project">
                                 <p class="card-text" id="description"> {{ item.description }} </p>
                                 <div class="media-buttons">
-                                    <a class="button" href="">Github</a>
-                                    <a class="button" href="">Go Live</a>
+                                    <a class="button" target="_blank" :href="item.github">Github</a>
+                                    <a class="button" target="_blank" :href="item.host">Go Live</a>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col" id="git">
+                        <a id="hub" href="">Check out my Github</a>
                     </div>
                 </div>
             </div>
@@ -60,6 +65,8 @@ h3 {
     width:100%;
     height:100%;
     display:flex;
+    height:2.7rem;
+    width:100%;
     justify-content: space-evenly;
 }
 
@@ -69,8 +76,33 @@ h3 {
     color:black;
     background: red;
     font-weight:bold;
+    text-align:center;
+    text-decoration: none;
     border:2px solid black;
     border-radius:10px;
+}
+
+#git{
+    display: flex;
+    justify-content: center;
+    align-items:center;
+}
+#hub{
+    text-decoration: none;
+    color:red;
+    border:2px solid black;
+    border-radius:10px;
+    background-color:black;
+    width:11rem;
+    text-align: center;
+    height:2.5rem;
+    padding:.2rem;
+    margin-bottom:2rem;
+}
+
+#hub:hover{
+    color:white;
+    background: red;
 }
 
 .button:hover{
